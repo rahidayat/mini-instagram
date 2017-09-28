da0b3623d0c0ec09a1f1204dc5ffd970e96bf18c
rahid <hidayat.85@gmail.com>
2017-09-28 16:25:58 +0700

    initial setup

---
+++ server/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+const logger = require('morgan');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://admin:123@cluster0-shard-00-00-jgxqq.mongodb.net:27017,cluster0-shard-00-01-jgxqq.mongodb.net:27017,cluster0-shard-00-02-jgxqq.mongodb.net:27017/mininsta?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin');
+
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(cors())
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening Port 3000');
+})
